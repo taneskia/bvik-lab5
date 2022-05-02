f820494396ff3a40e4b76e4cf917110e0bb8e904
Andrej Taneski <andrejtaneski7@gmail.com>
2022-05-02 16:27:41 +0200

    Create coin

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -13,9 +14,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+const { infuraApiKey, mnemonic, etherscanApiKey } = require('./secrets.json');
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infuraApiKey}`,
+      accounts: { mnemonic: mnemonic },
+    },
+  },
+  etherscan: {
+    apiKey: `${etherscanApiKey}`
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const FinkiCoin = await hre.ethers.getContractFactory("FinkiCoin");
+  const finkiCoin = await FinkiCoin.deploy();
 
-  await greeter.deployed();
+  await finkiCoin.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Finki Coin deployed to:", finkiCoin.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
